431efee482418fa804dd2c53203e8a30e8a7b7da
haruominomura <haruomi.nomura@gmail.com>
2026-01-04 22:53:11 +0900

    fe: create color schema

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="w-full h-auto p-3 bg-amber-400 flex gap-3">
+    <header className="w-full h-auto p-3 bg-brand-primary flex gap-3">
       <div className="flex items-center gap-3">
         <Icon name="cloudication" />
         <Image className="h-3 w-auto" src="img/vector/cloudication-logotype.svg" alt="クモ二ケーション" width={1} height={1}></Image>
