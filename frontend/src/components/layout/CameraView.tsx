cd01a90661e52a9447f3488aed7404f5b069b2a6
haruominomura <haruomi.nomura@gmail.com>
2026-01-10 16:47:05 +0900

    fe: create shutter animation

--- frontend/src/components/layout/CameraView.tsx
+++ frontend/src/components/layout/CameraView.tsx
@@ -22,6 +22,20 @@ const CameraView = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const capture = () => {
+    if (!videoRef.current) return;
+    camera.capture(videoRef);
+    shutterAnimation();
+  }
+
+  const shutterAnimation = async () => {
+    if (!videoRef.current) return;
+    videoRef.current!.style.filter = "brightness(.25)";
+    await setTimeout(() => {
+      videoRef.current!.style.filter = "brightness(1)";
+    }, 200);
+  }
+
   return (
     <div className="w-full h-[80vh] p-3 bg-bg-overlay rounded-[48px] overflow-clip relative flex flex-col justify-end gap-3">
       <video
@@ -29,7 +43,7 @@ const CameraView = () => {
         autoPlay
         playsInline
         muted
-        className="w-full h-full object-cover absolute top-0 left-0 z-0"
+        className="w-full h-full object-cover absolute top-0 left-0 z-0 transition-all duration-100"
       ></video>
       {camera.error && (
         <p className="text-red-600 text-sm text-center bg-bg w-fit px-4 py-3 rounded-full -translate-1/2 absolute top-24 left-1/2">
@@ -44,7 +58,7 @@ const CameraView = () => {
           className="bg-bg-overlay relative z-1 font-bold w-auto h-20 aspect-square backdrop-blur-md"
         />
         <Button
-          onClick={() => camera.capture(videoRef)}
+          onClick={capture}
           icon="camera"
           label="写真を撮る"
           className="relative z-1 font-bold w-full h-20"
